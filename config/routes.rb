0eac5898fcd2862b8b5d3bebc0b17a74ae882c97
David Stinnette <dastinnette@gmail.com>
2016-02-29 17:30:29 -0700

    Created users controller with show, created user model with find_or_create_from_auth method, migrated user model creation

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   resources :tweets, only: [:index, :show]
   resources :users, only: [:show]
-  
+
+  get '/auth/twitter', as: :login
   get '/auth/twitter/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,2 +1,9 @@
 FactoryGirl.define do
+  factory :user do
+    name "MyString"
+    screen_name "MyString"
+    uid "MyString"
+    oauth_token "MyString"
+    oauth_token_secret "MyString"
+  end
 end
