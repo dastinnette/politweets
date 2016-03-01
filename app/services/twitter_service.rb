9a4ae1afac2dfb8cd453e79e336a9b0b48c4e778
David Stinnette <dastinnette@gmail.com>
2016-03-01 11:34:18 -0700

    Add randomly generated secret_key_base stored in app.yml

--- app/services/twitter_service.rb
+++ app/services/twitter_service.rb
@@ -1,11 +1,11 @@
 class TwitterService
 
-  def initialize(user)
+  def initialize
     @client ||= Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV['TWITTER_KEY']
-      config.consumer_secret     = ENV['TWITTER_SECRET']
-      config.access_token        = user.oauth_token
-      config.access_token_secret = user.oauth_token_secret
+      config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+      config.access_token        = ENV['TWITTER_TOKEN']
+      config.access_token_secret = ENV['TWITTER_TOKEN_SECRET']
     end
   end
 
