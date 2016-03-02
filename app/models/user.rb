a3b8c413828d0311e4f7156049df834911c959a8
David Stinnette <dastinnette@gmail.com>
2016-03-01 19:28:34 -0700

    Refactor user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,16 @@
 class User < ActiveRecord::Base
 
-  def self.find_or_create_from_auth(auth_info)
-    where(uid: auth_info[:uid]).first_or_create do |new_user|
-      new_user.uid                = auth_info.uid
-      new_user.name               = auth_info.extra.raw_info.name
-      new_user.screen_name        = auth_info.extra.raw_info.screen_name
-      new_user.oauth_token        = auth_info.credentials.token
-      new_user.oauth_token_secret = auth_info.credentials.secret
-    end
+  def self.find_or_create_from_auth(auth)
+    user = User.find_or_create_by(uid: auth[:uid])
+
+    user.uid                = auth.uid
+    user.name               = auth.extra.raw_info.name
+    user.screen_name        = auth.extra.raw_info.screen_name
+    user.oauth_token        = auth.credentials.token
+    user.oauth_token_secret = auth.credentials.secret
+
+    user.save
+    user
   end
 
 end
