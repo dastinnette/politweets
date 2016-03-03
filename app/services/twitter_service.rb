a3552d7a09968f59d974aa89d18e6a414b4481a2
David Stinnette <dastinnette@gmail.com>
2016-03-02 17:13:32 -0700

    Refactor twitter service PORO

--- app/services/twitter_service.rb
+++ app/services/twitter_service.rb
@@ -12,28 +12,13 @@ class TwitterService
 
   # Make more dynamic to allow for searches
 
-  def bernie_tweets
+  def search_tweets
     tweets = client.search("to:#feelthebern",
                            result_type: "recent")
     filtered_tweets = tweets.select do |tweet|
       !tweet.user.location.is_a?(Twitter::NullObject)
     end
     filtered_tweets.each { |tweet| Tweet.create_from_search(tweet) }
-  end
-
-  def bernie_output
-    bernie_tweets.map do |tweet|
-      tweet.user.location
-    end
-  end
-
-  def trump_tweets
-    tweets = client.search("to:#MakeAmericaGreatAgain",
-                           result_type: "recent")
-    locations = tweets.select do |tweet|
-      !tweet.user.location.is_a?(Twitter::NullObject)
-    end
-    locations
   end
 
 end
