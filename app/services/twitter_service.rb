de7eaa8b28cf2d6393a5f2edcab559a10b9ee8f2
David Stinnette <dastinnette@gmail.com>
2016-03-07 12:54:16 -0700

    User model test passing

--- app/services/twitter_service.rb
+++ app/services/twitter_service.rb
@@ -11,7 +11,7 @@ class TwitterService
   end
 
   def search_tweets(hashtag, index)
-    tweets = client.search("to:#{hashtag}", result_type: "recent", count: 100)
+    tweets = client.search("to:#{hashtag}", result_type: "recent")
     filtered_tweets = tweets.select do |tweet|
       !tweet.user.location.is_a?(Twitter::NullObject)
     end
--- spec/models/hashtag_spec.rb
+++ spec/models/hashtag_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe Hashtag, type: :model do
 
   describe "Hashtag model testing" do
 
