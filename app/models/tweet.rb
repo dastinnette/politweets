ad2d328a1cf3ba438c8d8573eda6af7eacb843f2
David Stinnette <dastinnette@gmail.com>
2016-03-08 16:35:26 -0700

    Shoved a square peg into a round hole with Horace's guidance (made ruby AR talk to js highcharts via JSON...I think?)

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -91,6 +91,26 @@ class Tweet < ActiveRecord::Base
     end
   end
 
+  def self.v2
+    colors = {"Bernie" => '#303f9f', "Trump" => '#b71c1c'}
+    Tweet.all.group_by(&:state_code).map do |state, pile_o_tweets|
+      by_hashtag = pile_o_tweets.group_by(&:hashtag_id)
+      counts = {"Bernie" => by_hashtag[1].count,
+                "Trump" => by_hashtag[2].count}
+      winner = counts.max_by(&:last).first
+      loser = counts.min_by(&:last).first
+      total = (counts[loser] + counts[winner])
+      {state: state,
+        winner: winner,
+        loser: loser,
+        loserCount: counts[loser],
+        winnerCount: counts[winner],
+        color: colors[winner],
+        winnerPercentage: ((counts[winner]/total.to_f) * 100).round(1)
+      }
+    end
+  end
+
   private
 
   def self.state_codes
